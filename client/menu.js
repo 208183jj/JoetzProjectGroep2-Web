a9557cc85851a16968e411758eb0538a53c0e075
Jorian Jooris <jorian.jooris.t8183@student.hogent.be>
2016-08-22 19:42:30 +0200

    fix menu

--- client/menu.js
+++ client/menu.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Session } from 'meteor/session';
-Template.menu.rendered = function () {
-  $(".button-collapse").sideNav();
-};
+Template.menu.onRendered(function(){
+  $('.button-collapse').sideNav();
+})
